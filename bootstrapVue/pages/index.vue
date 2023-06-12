2360841271006f9fec4c5864a48625bf361d5cab
JiangShuuu <globelex65@gmail.com>
2023-06-12 21:23:30 +0800

    upd

---
+++ bootstrapVue/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>
+      <!-- <b-button >Launch demo modal</b-button> -->
+      <div v-b-modal.modal-1 class="btn-primary">BTN</div>
+
+      <b-modal id="modal-1" title="BootstrapVue">
+        <p class="my-4">Hello from modal!</p>
+      </b-modal>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
