f709554efa643dfedbe9623996b020775b9a0880
JiangShuuu <globelex65@gmail.com>
2023-06-12 22:41:47 +0800

    upd

--- bootstrapVue/pages/index.vue
+++ bootstrapVue/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center justify-center w-full h-screen">
-    <NuxtLink to="/popover" class="text-primary">PopoverPage</NuxtLink>
+    <NuxtLink to="/ui" class="text-primary">ComponentUI</NuxtLink>
   </div>
 </template>
 
---
+++ bootstrapVue/pages/ui.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex items-center justify-center w-full h-screen space-x-2">
+    <button id="popover-del" class="w-20 btn btn_primary">Popover</button>
+    <PopoverDel />
+    <button v-b-modal="`manage-modal`" class="w-20 btn btn_primary">Modal</button>
+    <ModalJobSort />
+    <button class="w-20 btn btn_primary" @click="showToast">Toast</button>
+    <button v-b-tooltip.hover="'Tooltip!'" class="w-20 btn btn_primary">Tooltip</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PopoverPage',
+  methods: {
+    showToast() {
+      this.$root.$bvToast.toast('BootstrapToast!!', {
+        noCloseButton: true,
+        toaster: 'b-toaster-top-center',
+        bodyClass: 'custom-toaster',
+        autoHideDelay: 2000,
+    })
+    }
+  }
+}
+</script>
