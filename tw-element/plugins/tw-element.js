b15d51c22188bca12a8e22a871727f286f135add
JiangShuuu <globelex65@gmail.com>
2023-06-18 10:15:54 +0800

    upd

--- tw-element/plugins/tw-element.js
+++
@@ -1,7 +0,0 @@
-// import Vue from 'vue'
-import { Modal, Tooltip, Toast, Dropdown, initTE } from 'tw-elements'
-
-initTE({ Modal, Tooltip, Toast, Dropdown })
-// Vue.prototype.$modal = Modal
-// Vue.prototype.$toast = Toast
-// Vue.prototype.$tooltip = Tooltip
