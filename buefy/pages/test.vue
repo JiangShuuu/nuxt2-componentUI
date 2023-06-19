ae9b347bb8f351d43fee55742b6782496f42a1ef
JiangShuuu <globelex65@gmail.com>
2023-06-19 10:33:56 +0800

    upd

---
+++ buefy/pages/test.vue
@@ -0,0 +1,37 @@
+<template>
+    <section>
+        <div class="buttons">
+            <!-- <b-button
+                label="Launch card modal (keep scroll)"
+                type="is-primary"
+                size="is-medium"
+                @click="isCardModalActive = true" /> -->
+                <div class="border" @click="open">BTN</div>
+        </div>
+        <div class="border" data-target="modal-js-example">
+            btn-2
+        </div>
+
+        <b-modal id="modal-js-example" :width="640" scroll="keep">
+            <div class="border bg-white">
+                <p class="text-4xl">dsadsada</p>
+            </div>
+        </b-modal>
+    </section>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                isImageModalActive: false,
+                isCardModalActive: false
+            }
+        },
+        methods: {
+            open() {
+                this.$buefy.modal.open('modaldsadsaa')
+            }
+        }
+    }
+</script>
---
+++ buefy/pages/ui.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center justify-center w-full h-screen space-x-2">
+    <button class="w-20 btn btn_primary">Popover</button>
+    <button class="w-20 btn btn_primary" @click="openModal">
+      Modal
+    </button>
+    <button class="w-20 btn btn_primary">Toast</button>
+    <button class="w-20 btn btn_primary">Tooltip</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentUI',
+  methods: {
+    openModal() {
+        this.$modal.show('test-modal')
+    }
+  },
+}
+</script>
